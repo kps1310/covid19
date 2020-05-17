b951d6cd0be010e4f3bcd3a2de16dd8b69ba1068
kps1310 <47588874+kps1310@users.noreply.github.com>
2020-05-17 21:23:41 +0530

    Updates

---
+++ precache-manifest.6b1f1c11bd05ce7248080e7b38eabe30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b8a1db75d55bba918fabcf03710cbf",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "533808df8dc4bf172600",
+    "url": "/covid19/static/css/main.23ebd1e4.chunk.css"
+  },
+  {
+    "revision": "73bfb783a25b95a24f3d",
+    "url": "/covid19/static/js/2.e5aec47f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.e5aec47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533808df8dc4bf172600",
+    "url": "/covid19/static/js/main.5b426330.chunk.js"
+  },
+  {
+    "revision": "080958ad2f3d2ab682fd",
+    "url": "/covid19/static/js/runtime-main.6dbe199c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/images.d7265326.png"
+  }
+]);
